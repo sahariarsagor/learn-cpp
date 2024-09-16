109dca4ebc9690d464ec22c72481a01615fc9459
Sahariar Sagor <contactsahariar@gmail.com>
2024-09-16 21:58:08 +0600

    excape sequence character

---
+++ escapeSequence.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main() {
+    //escape sequence character 
+    //to print a new line \n
+    cout << "Hello Sahariar Sagor \n";
+    cout << "How are you ? \n";
+
+    // use special one thing in escape sequence 
+    cout << "Sahariar Says \" Cpp is a easy to understand programming language. \" ";
+    return 0;
+}
