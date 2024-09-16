ba4967dc6f8f3bbb971f774a3da848284a07b156
Sahariar Sagor <contactsahariar@gmail.com>
2024-09-16 22:07:40 +0600

    variable

---
+++ variable.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main() 
+{
+    //Variable in CPP Programming: 
+    //int, char, string, bool, double;
+    //Declaring all type of variable: 
+    int myNumber = 21;
+    char myCharacter = 'S';
+    string myName = "Sahariar Sagor"; 
+    bool isPass = true;
+    double myPoint = 4.68;
+
+    cout << myNumber; //print the value of myNumber variable. 
+    cout << myCharacter; //print the value of myCharater variable. 
+    cout << myName; //print sahariar sagor which is the value of myName variable. 
+    cout << isPass; //print 1 for true and 0 for false.  which is the value of isPass variable which type is boolean. 
+
+    cout << myPoint; // double type variable contain large floating number. its just a example that i print 4.68; 
+
+    return 0;
+}
